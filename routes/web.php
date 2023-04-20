6778242a15addd371e4ff211226e95e9304673c7
Rishabhpanchal1408 <rishabhpanchal1408@gmail.com>
2023-04-20 12:01:04 +0530

    Added Documentation

--- routes/web.php
+++ routes/web.php
@@ -20,33 +20,70 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Get All Car Details
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/car', [CarsController::class, 'index']);
+
 // Brand
+
+// Brand Get All
 Route::get('/brand', [BrandController::class, 'index']);
+
+// Brand Insert
 Route::get('/brand/create', [BrandController::class, 'create']);
 Route::post('/brand', [BrandController::class, 'store']);
+
+// Brand Update
 Route::get('/brand/{brand}/edit', [BrandController::class, 'edit']);
 Route::put('/brand/{brand}', [BrandController::class, 'update']);
+
+// Brand Delete
 Route::delete('/brand/{brand}', [BrandController::class, 'destroy']);
+
+
 // Type
+
+// Type Get All
 Route::get('/type', [TypeController::class, 'index']);
+
+// Type Insert
 Route::get('/type/create', [TypeController::class, 'create']);
 Route::post('/type', [TypeController::class, 'store']);
+
+// Type Update
 Route::get('/type/{type}/edit', [TypeController::class, 'edit']);
 Route::put('/type/{type}', [TypeController::class, 'update']);
+
+// Type Delete
 Route::delete('/type/{type}', [TypeController::class, 'destroy']);
+
 // Color
+
+// Color Get All
 Route::get('/color', [ColorController::class, 'index']);
+
+// Color Insert
 Route::get('/color/create', [ColorController::class, 'create']);
 Route::post('/color', [ColorController::class, 'store']);
+
+// Color Update
 Route::get('/color/{color}/edit', [ColorController::class, 'edit']);
 Route::put('/color/{color}', [ColorController::class, 'update']);
+
+// Color Delete
 Route::delete('/color/{color}', [ColorController::class, 'destroy']);
+
 // Engine
+
+// Engine Get All
 Route::get('/engine', [EngineController::class, 'index']);
+
+// Engine Insert
 Route::get('/engine/create', [EngineController::class, 'create']);
 Route::post('/engine', [EngineController::class, 'store']);
+
+// Engine Update
 Route::get('/engine/{engine}/edit', [EngineController::class, 'edit']);
 Route::put('/engine/{engine}', [EngineController::class, 'update']);
+
+// Engine Delete
 Route::delete('/engine/{engine}', [EngineController::class, 'destroy']);
